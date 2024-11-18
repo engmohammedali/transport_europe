8ca8d33afdffc3ecc446f0361b3c2d1fae6f66b5
MahmoudAtro <mahmoudyatro@gmail.com>
2024-11-18 22:31:47 +0300

    - added styles files to project - added custom app_button and app_textfeild

---
+++ lib/core/theming/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:transport_europe/core/theming/colors.dart';
+import 'package:transport_europe/core/theming/font_weight_helper.dart';
+
+class TextStyles {
+  static TextStyle font14LightGrayRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorsManager.lightGray,
+  );
+
+  static TextStyle font14DarkBlueMedium = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: ColorsManager.darkBlue,
+  );
+}
